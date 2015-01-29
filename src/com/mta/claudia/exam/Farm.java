cb6db6b51c261338fcdab979920bd263bd2976be
claudia edelan <edelmanclaudia@gmail.com>
2015-01-29 15:50:21 +0200

    Exam q1 - Farm - works!!!!

--- src/com/mta/claudia/exam/Farm.java
+++ src/com/mta/claudia/exam/Farm.java
@@ -2,30 +2,42 @@ package com.mta.claudia.exam;
 
 public class Farm {
 
-	private float sum = 0;
+	private static float sum = 0;
 
 	public final static int SIZE = 5;
 
-	AbsAnimal[] animalArray = new AbsAnimal[SIZE];
+	static AbsAnimal[] animalArray = new AbsAnimal[SIZE];
 
-	//animalArray[0] = new Cow("milka",2,2,2);
-	//animalArray[1] = new Dog ("jo",2,2);
-	//animalArray[2] = new Hen ("kity",2,2,2);
-	//animalArray[3] = new Sheep ("doly",2,2,2);
-	//animalArray[4] = new Cow("loly",2,2,2);
+	public static void Farm(){
+		//animalArray = new AbsAnimal[SIZE];
+		animalArray[0] = new Cow("milka",2,2,2);
+		animalArray[1] = new Dog ("jo",2,2);
+		animalArray[2] = new Hen ("kity",2,2,2);
+		animalArray[3] = new Sheep ("doly",2,2,2);
+		animalArray[4] = new Cow("loly",2,2,2);
 
-}
-/*
-	for(int i = 0; i < SIZE; i++)
-	{
-		System.out.println("Name: " + getName(animalArray[i].name) + " ,Age" + getAge(animalArray[i].age));
-		walk();
+	}
 
-		if(animalArray[i] instanceof AbsAnimalMilk || animalArray[i] instanceof AbsAnimalMilk)
+	public static void fuck(){
+		Farm();
+		
+		for(int i = 0; i < SIZE; i++)
 		{
-			sum = sum + getMilk();
-			system.out.println("Total farm milk capacity is " + (int)sum + "letters");
+			System.out.println("Name: " + animalArray[i].getName() + " ,Age" + animalArray[i].getAge());
+			animalArray[i].walk();
+
+			if(animalArray[i] instanceof AbsAnimalMilk)
+			{
+				sum += ((AbsAnimalMilk) animalArray[i]).getMilk();
+				System.out.println("Total farm milk capacity is " + (int)sum + " letters");
+			}
 		}
 	}
- */
+	
+	public static void main (String []args)
+	{
+		fuck();
+		System.out.println("yes i can fuck " + animalArray[0].getName());
+	}
+}
 
